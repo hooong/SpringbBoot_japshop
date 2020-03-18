e3da153d56db57c0034ee1d88fbee9eb7e01c29e
hooong <tjrwns0529@gmail.com>
2020-03-18 16:49:15 +0900

    add stockquantity logic

--- src/main/java/jpabook/jpashop/dommain/item/Item.java
+++ src/main/java/jpabook/jpashop/dommain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.dommain.item;
 
 import jpabook.jpashop.dommain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)   // 하나의 테이블에 모두 때려박고 dtype으로 구분
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -22,8 +23,27 @@ public abstract class Item {
 
     private String name;
     private int price;
-    private int stockQuantitu;
+    private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
